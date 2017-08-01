4f4bdc82e287793910208a44fc93ba450df52b10
ricosmall <805163443@qq.com>
2017-08-01 17:00:20 +0800

    修改部署代码

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
 # 更新next主题
-echo '准备更新next主题\n===================='
+echo '准备更新next主题'
 cd themes/next
 git pull
 cd ../../
 echo '更新next主题成功！'
 
 # 清除之前生成的代码
-echo '准备清除缓存\n===================='
+echo '准备清除缓存'
 hexo clean
 echo '清除缓存成功！'
 
 # 生成
-echo '准备生成新的资源\n===================='
+echo '准备生成新的资源'
 hexo generate
 echo '生成新的资源成功！'
 
 # 部署（包含部署到Coding）
-echo '准备部署新的资源\n===================='
+echo '准备部署新的资源'
 hexo deploy
 echo '部署新的资源成功！'
 
 # push到github
-echo '更新GitHub源码\n===================='
+echo '更新GitHub源码'
 git add .
 git commit -m '更新文章'
 git push origin hexo
