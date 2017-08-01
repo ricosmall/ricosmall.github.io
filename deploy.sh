5b56cea14c58df69bd8e36be40933bfcba7b9437
ricosmall <805163443@qq.com>
2017-08-01 12:01:33 +0800

    修改发布文件

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git clone https://github.com/ricosmall/hexo-theme-next.git --depth=1 themes/next
 hexo clean
 hexo deploy --generate
 
-rm -r themes/next
+rm -rf themes/next
 git add .
 git commit -m '更新文章'
 git push origin hexo
